5c24ba48bd141c5ca30848a730a7b03c72a2a261
Gabriel Morato Villassanti <gabriel.villassanti2@hotmail.com>
2024-06-06 17:01:23 -0300

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const checkBtn = document.getElementById('check-btn');
+const inputText = document.getElementById('input-text');
+const result = document.getElementById('result');
+
+checkBtn.addEventListener('click', () => {
+    console.log('hi');
+})
