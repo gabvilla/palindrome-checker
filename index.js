165f59ff1c17ee8e1117c8cddcdf4acdcd37a8af
Gabriel Morato Villassanti <gabriel.villassanti2@hotmail.com>
2024-06-07 10:06:25 -0300

    added the logical check for unique words

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const checkBtn = document.getElementById('check-btn');
+const checkButton = document.getElementById('check-btn');
 const inputText = document.getElementById('input-text');
 const result = document.getElementById('result');
 
-checkBtn.addEventListener('click', () => {
+checkButton.addEventListener('click', () => {
     if (inputText.value == ""){
         window.alert("Please input a value");
     } else {
         const inputString = inputText.value;
+        const inputArray = inputString.split('');
+        const outputString = inputArray.toString().replace(/,/g,"");
+        console.log(inputString + " " + outputString)
+        if (inputString === outputString){
+            result.innerText = `${inputString} it's a palindrome!`
+        } else {
+            result.innerText = `${inputString} it's not a palindrome.`
+        }
     };
 })
