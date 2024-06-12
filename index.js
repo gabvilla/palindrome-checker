e5354c0300785222a960f3248c8be1897bd1c8a3
Gabriel Morato Villassanti <gabriel.villassanti2@hotmail.com>
2024-06-12 14:25:48 -0300

    adjusted the check logic

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ checkButton.addEventListener('click', () => {
     } else {
         const inputString = inputText.value;
         const stringArray = inputString.replace(/\s/g, '').replace(/[^\w\s]/gi, '').split('');
-        const reversedArray = stringArray.reverse();
-    };
+        const reversedString = stringArray.reverse().join().replace(/[^\w\s]/gi, '');
+        const originalString = stringArray.join().replace(/[^\w\s]/gi, '');
+        console.log(originalString + ' ' + re)
+        if (originalString == reversedString) {
+            result.innerText = `${inputString} it's a palindrome!`
+        } else {
+            result.innerText = `${inputString} it's not a palindrome.`
+        }
+    }
 })
