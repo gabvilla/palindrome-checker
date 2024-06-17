329fa205db5700983470c89746c61df58e4cd7d2
Gabriel Morato Villassanti <gabriel.villassanti2@hotmail.com>
2024-06-17 16:19:58 -0300

    created e regex variable and modified the strings methods

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const checkButton = document.getElementById('check-btn');
-const inputText = document.getElementById('input-text');
+const inputText = document.getElementById('text-input');
 const result = document.getElementById('result');
+const regEx = /[^a-zA-Z 123456789]/g;
 
 checkButton.addEventListener('click', () => {
     if (inputText.value == ""){
         window.alert("Please input a value");
     } else {
         const inputString = inputText.value;
-        const stringArray = inputString.replace(/\s/g, '').replace(/[^\w\s]/gi, '').split('');
-        const reversedString = stringArray.reverse().join().replace(/[^\w\s]/gi, '');
-        const originalString = stringArray.join().replace(/[^\w\s]/gi, '');
-        console.log(originalString + ' ' + re)
+        const stringArray = inputString.replace(/\s/g, '').replace(regEx, '').split('');
+        const reversedString = stringArray.reverse().join().replace(regEx, '').toLowerCase();
+        const originalString = stringArray.reverse().join().replace(regEx, '').toLowerCase();
+        console.log(originalString + ' ' + reversedString);
         if (originalString == reversedString) {
-            result.innerText = `${inputString} it's a palindrome!`
+            result.innerText = `${inputString} is a palindrome!`
         } else {
-            result.innerText = `${inputString} it's not a palindrome.`
+            result.innerText = `${inputString} is not a palindrome.`
         }
     }
 })
